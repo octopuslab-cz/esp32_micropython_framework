5dd070dfd10d6162556bb809d58bc93d2840eee7
Honza S. Copak <bonum.comunae@volny.cz>
2023-01-27 09:55:50 +0100

    typo: add target

--- mip_install.py
+++ mip_install.py
@@ -1,11 +1,9 @@
 """
 octopuslab-cz: esp32_micropython_framework/package.json
 
-"components/led/__init__.mpy"
-Copying: /lib/components/rgb/__init__.mpy
-
-"/components/led/__init__.mpy"
-Copying: components/led/__init__.mpy
+target="." -> 
+Copying: ./components/led/__init__.mpy
+...
 """
 
 from time import sleep
@@ -16,11 +14,9 @@ wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 sleep(5)
 
-
 print("wifi connect")
 wlan.connect('ssid', 'password')
 sleep(5)
 
 print("[github:octopuslab-cz/esp32_micropython_framework]")
-mip.install("github:octopuslab-cz/esp32_micropython_framework")
-
+mip.install("github:octopuslab-cz/esp32_micropython_framework", target=".")
