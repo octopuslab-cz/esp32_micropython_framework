eae706faf29400701ba297f553cfc4f43b905935
Honza S. Copak <bonum.comunae@volny.cz>
2023-07-17 20:34:14 +0200

    typo,  clean up

--- config/nvs.py
+++ config/nvs.py
@@ -1,26 +1,26 @@
 import json
-import esp32
+from esp32 import NVS
 
-NEMESPACE = "octopus"
+NAMESPACE = "octopus"
 
 
 def set_config_from_file(source):
     with open(source, 'r') as f:
         config_str = f.read()
-    s = esp32.NVS(NEMESPACE)
+    s = NVS(NAMESPACE)
     s.set_blob('config', config_str)
     s.commit()
 
 
 def set_config(config_obj):
     config_str = json.dumps(config_obj)
-    s = esp32.NVS(NEMESPACE)
+    s = NVS(NAMESPACE)
     s.set_blob('config', config_str)
     s.commit()
 
 
 def get_config():
-    s = esp32.NVS(NEMESPACE)
+    s = NVS(NAMESPACE)
     # fake read to determine size
     config_str = bytearray()
     data_length = s.get_blob('config', config_str)
