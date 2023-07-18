a65b00892fe3a0353d26b9a6d4ceeb7727b36d24
Honza S. Copak <bonum.comunae@volny.cz>
2023-07-18 15:52:24 +0200

    simple components test - refactor.

--- tests/test_components.py
+++ tests/test_components.py
@@ -1,16 +1,52 @@
 # (c) OctopusLAB 2016-23 - MIT
 
+ERR = "[ == Error = ]"
+
+try:
+    from utils.pinout import get_device_config
+    print(get_device_config())
+    print()
+    print("-"*29,"\n")
+except:
+    print(ERR + " get_device_config")
+    print("(try: setup() > ds)\n")
+
 try:
     import examples.led_class
 except:
-    print("[ Error ] led_class")
+    print(ERR + " led_class\n")
+
+try:
+    import examples.rgb_ws
+except:
+    print(ERR + " rgb_ws\n")
 
 try:
     import examples.pwm
 except:
-    print("[ Error ] pwm")
+    print(ERR + " pwm\n")
+
+try:
+    import examples.display_lcd2
+except:
+    print(ERR + " display_lcd2\n")
+
+try:
+    import examples.display4_show
+except:
+    print(ERR + " display4_show\n")
+    
+try:
+    import examples.display8_show
+except:
+    print(ERR + " display8_show\n")
 
 try:
     import examples.display_oled
 except:
-    print("[ Error ] display_oled")
+    print(ERR + " display_oled\n")
+    
+try:
+    import examples.temperature_ds
+except:
+    print("[ == Error = ] temperature_ds\n")
