785b7e18f52b5b3f563dd2b6305cbedd07ccc43d
Honza S. Copak <bonum.comunae@volny.cz>
2023-07-28 16:58:21 +0200

    add new methods for eeprom

--- components/i2c_eeprom_24xxx.py
+++ components/i2c_eeprom_24xxx.py
@@ -1,30 +1,12 @@
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 from time import sleep_ms
-from machine import Pin, I2C
-
 """
 from components.i2c_eeprom_24xxx import EEPROM24x
-i2c = I2C(0, scl=Pin(1), sda=Pin(0), freq=400000)
-i2c_address = i2c.scan()
-print(i2c_address) #i2c_address = 7_bit_address_of_your_device
-
-EEPROM_DEVICE = "24x256"
-#EEPROM_DEVICE = "24x512"
-memory = EEPROM24x(i2c, 80, "24x08") 
-
-# Write an int of value 115 to address 12345
-memory.write_byte(12345, 115)
-
-# Now read it back and print it
-read_value = memory.read_byte(12345)
-
-print(read_value)
-
-old tests:
-def eeprom_read_block(addr, num=8):
-    data = i2c.readfrom_mem(EEPROM_ADDR, addr, num)
-    return data
+i2c = i2c_init()
+EEPROM_ADDR, EEPROM_DEVICE = 80, "24x16"
+eeprom = EEPROM24x(i2c, EEPROM_ADDR, EEPROM_DEVICE)
+data_256 = eeprom.read_block(num=256)
 """
 
 
@@ -36,10 +18,8 @@ class EEPROM24x:
         self.i2c = i2c
         # self.i2c_address = i2c_address[0]
         self.i2c_address = i2c_address # exactly
-        self.addrsize = 8
-
+        self.addrsize = 8 # self.addrsize = 16
         if(EEPROM_device == "24x02"):  self._MAX_ADDRESS  = int(2048/8)
-        # self.addrsize = 16
         elif(EEPROM_device == "24x04"): self._MAX_ADDRESS = int(4096/8)
         elif(EEPROM_device == "24x08"): self._MAX_ADDRESS = int(8192/8)
         elif(EEPROM_device == "24x16"): self._MAX_ADDRESS = int(16384/8) # 2048
@@ -49,7 +29,7 @@ class EEPROM24x:
             raise ValueError("Choose a device")
             return()
 
-    # ================ 
+    # ====
 
     def write_byte(self, address, data):
         if((address > self._MAX_ADDRESS) or (address < 0)):
@@ -74,9 +54,43 @@ class EEPROM24x:
         self.data_read = int.from_bytes(self.data_read, "big")
         return(self.data_read)
         """
-
         data = self.i2c.readfrom_mem(self.i2c_address, address, 1, addrsize=self.addrsize)
         return data
+
+
+    def fill_block(self, data=255,start_addr=0,num=128):
+        for i in range(num):
+            self.write_byte(start_addr+i, data)
+            # print(start_addr+i,data,hex(data))
+        
+    
+    def write_array(self, arr,start_addr=0):
+        # print("len",len(arr))
+        addr = start_addr
+        for data in arr:
+            self.write_byte(addr, int(data))
+            # print(addr,data,hex(data))
+            addr += 1
+ 
+ 
+    def write_string(self, s2w,start_addr=0):
+        # print("len",len(s2w))
+        addr = start_addr
+        for s in s2w:
+            # data = bytearray([ord(s)]) # bytearray([30+i*2])
+            data = ord(s) # byte > int!
+            self.write_byte(addr, data)
+            addr += 1
+        
+
+    def read_block(self, num=256): # per 1 Byte
+        data_block = bytearray(num)
+        byte = bytearray(1)
+        for i in range(num): # 8*8=64 // 16*8=128 //
+            byte = self.read_byte(i)
+            data_block[i] = byte[0]
+        return data_block
+
 
     def read_bytes(self, address, num_bytes=1):
         if((address > self._MAX_ADDRESS) or (address < 0)):
@@ -91,4 +105,3 @@ class EEPROM24x:
         self.data8 = self.i2c.readfrom_mem(self.i2c_address, address, num_bytes, addrsize=self.addrsize)
         #print("="*12,self.data8)
         return self.data8
-
