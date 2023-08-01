358210760782f09551488a54e089857228a1fc8c
Honza S. Copak <bonum.comunae@volny.cz>
2023-08-01 15:03:25 +0200

    add mip installer for uPy v. 1.19 (if necessary)

--- examples/display4_show.py
+++ examples/display4_show.py
@@ -3,11 +3,18 @@
 
 from time import sleep
 from machine import Pin
+from utils.pinout import set_pinout
 from components.display4 import TM1637Decimal
 
 
+print("[--- init ---] pinout")
+pinout = set_pinout()
+
 print("[--- init ---] display4")
-tm = TM1637Decimal(clk=Pin(18), dio=Pin(19))
+# tm = TM1637Decimal(clk=Pin(18), dio=Pin(19)) # DoIt
+# tm = TM1637Decimal(clk=Pin(6), dio=Pin(4))   # C3
+tm = TM1637Decimal(clk=Pin(pinout.SPI_CLK_PIN), dio=Pin(pinout.SPI_MISO_PIN))
+
 tm.show("----")
 
 for i in range(7):
@@ -15,4 +22,13 @@ for i in range(7):
     sleep(0.3)
 
 tm.show('abcd')
+sleep(2)
+
+for i in range(6):
+    tm.show('1234')
+    sleep(1)
+    tm.show('12.34')
+    sleep(1)
+    print(i)
+
 print("[-- finish --]\n")
--- examples/led_class.py
+++ examples/led_class.py
@@ -1,10 +1,16 @@
 # (c) OctopusLAB 2016-23 - MIT
 
 from time import sleep_ms
+from utils.pinout import set_pinout
 from components.led import Led
 
+
+print("[--- init ---] pinout")
+pinout = set_pinout()
+
+
 print("[--- init ---] led")
-led = Led(2)
+led = Led(pinout.BUILT_IN_LED) # 2
 
 print("[--- test ---] blink 3x")
 for blink in range(3):
