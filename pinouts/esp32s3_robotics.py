72aa9dcbebd3067836293ad8350efb4b02fa6d0e
Honza S. Copak <bonum.comunae@volny.cz>
2025-09-09 14:49:07 +0200

    update pinout s3

--- pinouts/esp32s3_robotics.py
+++ pinouts/esp32s3_robotics.py
@@ -18,18 +18,27 @@ SPI_MISO_PIN = const(13)
 SPI_MOSI_PIN = const(11)
 SPI_CS0_PIN  = const(10)
 
+#PWM
+P1_PIN = const(21)
+P2_PIN = const(47)
+P3_PIN = const(9)
+P4_PIN = const(15)
+P5_PIN = const(16)
+P6_PIN = const(3)
+P7_PIN = const(48) # LED2_PIN
+P8_PIN = const(14) # LED1_PIN
 
 #ANALOG_PIN
 BATMES_PIN = const(5)
 
-PWM1_PIN = const(21)
-PWM2_PIN = const(47)
-PWM3_PIN = const(9)
+# backward pin compatibility
+PWM1_PIN = const(21) # P1 
+PWM2_PIN = const(47) # P2
+PWM3_PIN = const(9)  # P3
 
-LED2_PIN = const(14)
-LED3_PIN = const(48)
+LED1_PIN = const(14)
+LED2_PIN = const(48)
 WSLED_PIN = const(38)
-
 
 D1_PIN = const(16)
 D2_PIN = const(15)
@@ -38,4 +47,3 @@ D3_PIN = const(3)
 # UART 1
 RXD1 = const(18)
 TXD1 = const(17)
-
